820bd880dd0f3dcc0fc87106abd77894fd1522b5
Jason Jacques <j.jacques.03@gmail.com>
2024-12-22 10:51:35 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jason Jacques on 2024-12-18.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
